2ae8f9b4f8df264f24e67d16013f6fbe528e29b4
ChristyTheresaChacko <christychacko9856@gmail.com>
2025-06-10 15:45:57 +0530

    Completed trial Portfolio. Dark mode not implemented

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
-const hamburger = document.querySelector("#hamburger");
-const menu = document.querySelector("#menu");
-const h1 = document.querySelector("#h1");
-const h2 = document.querySelector("#h2");
-const h3 = document.querySelector("#h3");
+const hamburger = document.querySelector("#hamburger")
+const menu = document.querySelector("#menu")
+const h1 = document.querySelector("#h1")
+const h2 = document.querySelector("#h2")
+const h3 = document.querySelector("#h3")
+const hlinks=document.querySelectorAll("#hlink")
+const body=document.querySelector("body")
+const moon=document.querySelector("#moon")
 
 
 hamburger.addEventListener("click",()=>{
@@ -10,4 +13,20 @@ hamburger.addEventListener("click",()=>{
     h1.classList.toggle("bg-white")
     h2.classList.toggle("bg-white")
     h3.classList.toggle("bg-white")
+})
+
+hlinks.forEach(link=>{
+    link.addEventListener("click",()=>{
+    menu.classList.toggle("hidden")
+    h1.classList.toggle("bg-white")
+    h2.classList.toggle("bg-white")
+    h3.classList.toggle("bg-white")
+    
+})
+})
+
+
+moon.addEventListener("click",()=>{
+    body.classList.toggle("dark")
+   
 })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
    module.exports = {
+    darkMode:"class",
      theme: {
        extend: {
-         screens: {
-           'sm': '480px',
+        spacing:{
+            "big":"48rem"
+        }
+        
+       },
+        screens: {
+           sm:"480px",
+           md:"767px",
+           lg:"1024px",
+           xl:"1024px",
            // other breakpoints if needed
          },
-       },
        fontFamily:{
         nunito:['Nunito','sans-serif']
        }
      },
-   };
+   }
